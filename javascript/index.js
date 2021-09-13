3db00c1fdccab9e57c00736d0080a30382cd01fc
MdMajor <memdmajor@gmail.com>
2021-09-13 23:03:35 +0530

    fixed main_page.html issue
    deleted show.js file and added two separate files for each index.html and main_page.html

---
+++ javascript/index.js
@@ -0,0 +1,51 @@
+let image = document.getElementById("image");
+let images = [
+  "img/avatar1.png",
+  "img/avatar2.png",
+  "img/avatar3.png",
+  "img/avatar4.png",
+  "img/avatar5.png",
+  "img/avatar6.png",
+  "img/avatar7.png",
+];
+
+//image change function
+
+setInterval(function () {
+  let random = Math.floor(Math.random() * 7);
+  image.src = images[random];
+}, 1200);
+
+function myFunction() {
+  var name = document.forms["myForm"]["nameid"].value;
+  var date = document.forms["myForm"]["main_date"].value;
+  console.log(date);
+
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (date == "") {
+    alert("Date Of Birth must be filled out");
+    return false;
+  } else {
+    //--------------passing data using new Window--------
+
+    // var newwin = window.open("main_page.html");
+    // newwin.onload = function(){
+    //   // "this" refers to newwin
+    //   this.name = name;
+    //   this.date = date;
+    // };
+
+    //-----------------Using Local Storage to save variable---------------------------------
+
+    // (SAVE TO LOCAL STORAGE
+    // localStorage.setItem("KEY", "VALUE");
+    localStorage.setItem("date", date);
+
+    // REDIRECT OR OPEN NEW WINDOW IF YOU WANT
+    window.open("main_page.html");
+    // location.href = "main_page.html";
+  }
+}
---
+++ javascript/main_page.js
@@ -0,0 +1,55 @@
+var noOfDays = document.getElementById("earthrotation");
+var dobHours = document.getElementById("dob-hours");
+
+var Difference_In_Time;
+var Difference_In_Days;
+
+var date2 = new Date();
+
+// To calculate the time difference of two dates
+
+function calDate() {
+  var date = localStorage.getItem("date");
+    // (EXTRA) TO CLEAR
+    // localStorage.removeItem("KEY");
+    // localStorage.clear();
+
+    Difference_In_Time = date2.getTime() - new Date(date).getTime();
+
+  // To calculate the no. of days between two dates
+  Difference_In_Days = parseInt(Difference_In_Time / (3600 * 1000 * 24));
+
+  let hours = Difference_In_Days * 24;
+  let minutes = hours * 60;
+  let seconds = minutes * 60;
+
+  setInterval(function () {
+    seconds = seconds + 1;
+    dobHours.innerHTML =
+      hours +
+      " hours " +
+      minutes +
+      " minutes and " +
+      seconds +
+      " sec have passed since you were born :)";
+  }, 1000);
+
+  setInterval(function () {
+    minutes = minutes + 1;
+    dobHours.innerHTML =
+      hours +
+      " hours " +
+      minutes +
+      " minutes and " +
+      seconds +
+      " sec have passed since you were born :)";
+  }, 60000);
+
+  //To display the final no. of days (result)
+  noOfDays.innerHTML =
+    Difference_In_Days + " days have passed since you were born :)";
+}
+
+setInterval(function () {
+  calDate();
+}, 1000);
